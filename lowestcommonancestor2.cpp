963386eed2261a2cb3f65f81b75c6f7d23017307
devved12 <devm4387@gmail.com>
2023-06-12 00:36:32 +0530

    logn lca

---
+++ lowestcommonancestor2.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define int long long
+
+// here i am posting O(logn)
+vector<int>grp[200005];
+int up[200005][20];
+int level[200005];
+
+void dfs(int src,int par,int lvl){
+    level[src]=lvl;
+    for(auto child:grp[src]){
+        if(child!=par){
+            dfs(child,src,lvl+1);
+        }
+    }
+}
+
+void binarylift(int src,int par){
+    up[src][0]=par;
+    for(int i=1;i<=19;i++){
+        if(up[src][i-1]!=-1){
+            up[src][i]=up[up[src][i-1]][i-1];
+        }
+        else{
+            up[src][i]=-1;
+        }
+    }
+
+    for(auto child:grp[src]){
+        if(child!=par){
+            binarylift(child,src);
+        }
+    }
+}
+
+int liftnode(int node,int level){
+    for(int i=0;i<=19;i++){
+        if(node==-1 || level==0)break;
+        int check=level&(1<<i);
+        if(check){
+          node=up[node][i];
+        }
+    }
+    return node;
+}
+
+int LCA(int u,int v){
+    if(level[u]<level[v]){
+        swap(u,v);
+    }
+    u=liftnode(u,level[u]-level[v]);
+    if(u==v)return u;
+    for(int i=19;i>=0;i--){
+        if(up[u][i]!=up[v][i]){
+            u=up[u][i];
+            v=up[v][i];
+        }
+        
+    }
+   return liftnode(u,1);
+
+}
+
+void solve(){
+   int n,q;cin>>n>>q;
+   for(int i=2;i<=n;i++){
+    int a;cin>>a;
+    grp[a].push_back(i);
+   }
+   dfs(1,-1,0);
+   binarylift(1,-1);
+
+   while(q--){
+    int a,b;cin>>a>>b;
+    cout<<LCA(a,b)<<"\n";
+   }
+}
+
+int32_t main(){
+  int t=1;
+  //cin>>t;
+  while(t--){
+    solve();
+  }
+}
